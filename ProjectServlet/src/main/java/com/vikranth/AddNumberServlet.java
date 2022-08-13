5805ef0660c919a43918519c69ea92ddbe913b38
gitallstara <avr2820@gmail.com>
2022-08-13 14:51:21 -0500

    Pushing small changes

--- ProjectServlet/src/main/java/com/vikranth/AddNumberServlet.java
+++ ProjectServlet/src/main/java/com/vikranth/AddNumberServlet.java
@@ -40,7 +40,6 @@ public class AddNumberServlet extends HttpServlet {
 		
 		
 		//Now just redirecting the client request to ohter servlet
-		
 		res.sendRedirect("sqr?k="+k);  // url  rewriting
 		
 		
